e69360960474887112e6714d4e4cf6aaaadc819a
BabyBoy-Yuan <zhiboyuan666@gmail.com>
2022-03-03 15:21:58 +0800

    新功能增加
    根据教程完成到第4章

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,25 @@
 from django.urls import path
 
 from . import views
+app_name = "polls"
+# urlpatterns = [
+#     # es: /polls/
+#     path('', views.index, name='index'),
+#     # es: /polls/5
+#     path('<int:question_id>/', views.detail, name='detail'),
+#     # es: /polls/5/result
+#     path('<int:question_id>/result/', views.results, name='results'),
+#     # es: /polls/5/vote/
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+# ]
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # es: /polls/
+    path('', views.IndexView.as_view(), name='index'),
+    # es: /polls/5
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # es: /polls/5/result
+    path('<int:pk>/result/', views.ResultsView.as_view(), name='results'),
+    # es: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
